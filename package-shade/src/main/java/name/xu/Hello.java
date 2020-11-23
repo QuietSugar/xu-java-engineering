83243a0f9fc5b9154d18b7f54b2aeaf3716c938d
xu <2542610526@qq.com>
2020-11-23 15:32:45 +0800

    maven-shade-plugin 打包成可执行的jar

---
+++ package-shade/src/main/java/name/xu/Hello.java
@@ -0,0 +1,15 @@
+package name.xu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class Hello {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
